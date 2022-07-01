6208eeeaf5770fb2a101629339edf2f08d8522d5
AJenkinsSoultions <ajenkinsuiw@gmail.com>
2022-07-01 12:28:34 -0500

    Add prototype file, Add paddle class, Add ball class, Add paddle user input, Add, paddle Boundry, Add ball inital Mvment, Add Screen, Add Main loop, Add time module

---
+++ turtle_test.py
@@ -0,0 +1,86 @@
+from turtle import *
+import time
+import random 
+#Screen Configuration
+window = Screen()
+window.setup(858, 525)
+window.bgcolor('grey')
+window.title('test')
+window.tracer(0)
+window.update()
+
+
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape('circle')
+        self.color('blue', 'cyan')
+        self.penup()
+        
+        self.setpos(0,-200)
+        self.speed('fastest')
+        
+    def move(self):
+        #inital movement
+        self.setx(self.xcor() + 2)
+        self.sety(self.ycor() + 2)
+        
+        
+        
+        
+
+class Paddle(Turtle):
+    def __init__(self):
+        super().__init__()    
+        self.shape('square')
+        self.color('blue')
+        self.penup()
+        
+        self.speed('fastest')
+        self.shapesize(1,8,1)
+        self.setpos(0,-220)
+        
+        #boundry
+        self.right_bounds = (330, 220)
+        self.left_bounds = (-350, -220)
+        
+    def move_left(self):
+        if self.pos() >= self.left_bounds:
+            x = self.xcor()
+            x -= 20
+            self.setx(x)
+        
+    def move_right(self):
+        if self.pos() <= self.right_bounds:
+            x = self.xcor()
+            x += 20
+            self.setx(x)
+        
+        
+
+p = Paddle()
+b = Ball()
+window.update()
+
+game_on = True
+
+while game_on:
+    window.update()
+    time.sleep(0.01)
+    
+    b.move()
+    
+    
+    
+    window.listen()
+    window.onkey(p.move_left, 'Left')
+    window.onkey(p.move_right, 'Right')
+    
+    window.update()
+
+
+    
+    
+    
+    
+    
